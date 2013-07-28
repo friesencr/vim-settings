a9ef376a09ec0551c0dc73afb3257e029fbcbe0c
Chris Friesen <friesencr@gmail.com>
2013-07-27 23:58:24 -0500

    better install

--- install.sh
+++ install.sh
@@ -3,7 +3,20 @@ hg clone https://vim.googlecode.com/hg/ vim
 cd vim
 hg pull
 hg update
-./configure --enable-rubyinterp  --enable-pythoninterp --enable-cscope  --enable-luainterp --enable-python3interp --with-features=huge --enable-multibyte --enable-xim --enable-gui=auto
+./configure \
+  --with-features=huge \
+  --with-x \
+  --enable-rubyinterp \
+  --enable-gtk2-check \
+  --enable-gnome-check \
+  --enable-pythoninterp \
+  --enable-luainterp \
+  --enable-python3interp \
+  --enable-pythoninterp \
+  --enable-multibyte \
+  --enable-xim \
+  --enable-gui=auto
+
 make
 sudo make install
 cd ..
